b3cb0f3342c8d610daec8a8717a45380730556e0
KAIFDK <mdkaifdoddamani@gmail.com>
2026-01-17 16:28:16 +0530

    Refresh Token and Acess Token

--- models/User.js
+++ models/User.js
@@ -171,6 +171,25 @@ userSchema.methods.comparePin = async function comparePin(candidatePin){
     return isMatch;
 }
 
+userSchema.methods.createAccessToken = function(){
+    return jwt.sign(
+        {userId:this._id,email:this.email},
+        process.env.JWT_SECRET,
+        {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.createRefreshToken = function(){
+    return jwt.sign(
+        {userId:this._id,email:this.email},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+
+
+
 
 const User = mongoose.model("User",userSchema);
 export default User;
