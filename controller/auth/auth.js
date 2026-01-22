cd121c4b0dc7fcab790bcebe859471884881f25c
KAIFDK <mdkaifdoddamani@gmail.com>
2026-01-22 11:54:38 +0530

    created login function

--- controller/auth/auth.js
+++ controller/auth/auth.js
@@ -34,3 +34,57 @@ const register = async (req,res) => {
         throw new BadRequestError('Error during registration');
     }
 }
+
+const login = async (req,res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        throw new BadRequestError('Please provide all values');
+    }
+
+    const user = await user.findOne({email});
+    if(!user){
+        throw new BadRequestError('Invalid Credentials');
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect){
+        let message;
+
+        if(user.blocked_until_password && user.blocked_until_password > Date.now()){
+            const remainingTime = Math.ceil((user.blocked_until_password - Date.now()) / (60 * 1000));
+            message = `Invalid Login Attempts. Try again after ${remaingTime} minutes`;
+    }
+    else{
+        const attemptsRemaing = 3 - user.wrong_password_attempt;
+        message = 
+        attemptsRemaing > 0
+        ? `Invalid Credentials. You have ${attemptsRemaing} more attempt(s) `
+        : "Invalid login attempts. Try after 30 minutes";
+    }
+        throw new UnauthenticatedError(message);
+}
+
+const access_token = user.createAccessToken();
+const refresh_token = user.createRefreshToken();
+
+let phone_exsit = false;
+let login_pin_exsit = false;
+if(user.phone_number){
+    phone_exsit = true;
+}
+if(user.login_pin){
+    login_pin_exsit = true;
+}
+
+res.status(StatusCodes.OK).json({user: {name : user.name ,email: user.email , userId: user.id, phone_exsit, login_pin_exsit},tokens : {access_token, refresh_token}});
+}
+
+const refresh_token = async (req,res) => {
+
+}
+
+async function generateRefreshToken(userId){
+    
+}
+
+export {register, login};
