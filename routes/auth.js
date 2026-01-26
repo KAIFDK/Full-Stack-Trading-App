ca0e5105183b13f9dc8995536960e8f75ae62827
KAIFDK <mdkaifdoddamani@gmail.com>
2026-01-27 02:56:45 +0530

    Created 4 api's

--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,11 @@
 import express from 'express';
+import { refreshToken, logout } from '../controller/auth/auth.js';
+import { authenticateUser } from '../MiddleWare/authentication.js';
 const router = express.Router();
+
+router.post("/refresh-token", refreshToken);
+router.post("/logout", authenticateUser, logout);
+router.post("/register", register);
+router.post("/login", login);
+
+export default router;
